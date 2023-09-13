1c78b9ebbd10484963bd93931259fd27b1e94ef5
Akira Moroo <akiram@ricsec.co.jp>
2023-09-13 12:13:21 +0900

    test: Use static link for building tests
    Signed-off-by: Akira Moroo <akiram@ricsec.co.jp>

--- test/algorithms/eclipser/fuzz_monoton.cpp
+++ test/algorithms/eclipser/fuzz_monoton.cpp
@@ -16,7 +16,6 @@
  * along with this program.  If not, see http://www.gnu.org/licenses/.
  */
 #define BOOST_TEST_MODULE algorithms.eclipser.fuzz_monoton
-#define BOOST_TEST_DYN_LINK
 #include <config.h>
 #include <random>
 #include <array>
